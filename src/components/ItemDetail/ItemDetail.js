8d358d19b20d50d27511d167a3553564082b985f
mativazque <97925060+mativazque@users.noreply.github.com>
2022-05-19 21:35:58 -0300

    Pre-entrega Proyecto Final

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -6,7 +6,7 @@ export default function ItemDetail({ item }) {
         <>
             <h1 className="titleHeader p-5">{item.title}</h1>
             <div className="text-center">
-                <img src={routeIMG + item.image} alt="ItemOne" />
+                <img src={routeIMG + item.image} alt="ItemOne" className="imgDetailItem"/>
             </div>
             <p className="text-white p-4 text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati beatae fugit nulla, maiores, accusamus exercitationem, dicta at voluptatem eum ab quisquam totam inventore? Odio, saepe recusandae voluptas laudantium eius dolorem!</p>
             <h5 className="text-center text-white p-4 m-0">Price: USD {item.price}</h5>
